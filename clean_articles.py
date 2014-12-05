0104d3fa19349960adc0cb47216fb844daaca0f5
jombooth <josephbooth@college.harvard.edu>
2014-12-05 14:44:42 -0500

    feature vecs

--- clean_articles.py
+++ clean_articles.py
@@ -1,59 +1,47 @@
 from textstat.textstat import textstat as ts
 from sklearn import svm
+from sklearn.feature_extraction import text
 
-import csv, enchant, string, pickle
+import csv, enchant, string, pickle, re
 from collections import Counter
-setpickle = False
-hh_words = []
 
-LEN_BOUND = 350
-
-try:
-    pfd = open('hh_nondict_words.pickle', 'rb')
-    hh_words = pickle.load(pfd)
-except:
-    print "*******RUN THIS SCRIPT A SECOND TIME AFTER IT COMPLETES********.\n\n"
-    pfd = open('hh_nondict_words.pickle', 'wb')
-    setpickle = True
+CHAR_LEN_BOUND = 1500
 
 fd = open('articles.csv', 'rb')
-rows = []
+raw_rows = []
 
 reader = csv.reader(fd, delimiter=',')
 for reading in reader:
-    rows.append(reading)
+    raw_rows.append(reading)
 
-rejectlist = []
+def basic_strip(article):
+    exclude = ["facebook", "digg", "tagged", "recommended", "stumbleupon", "share", "blogs", "user agreement", "subscription", "login", "twitter", "topics", "excel", "accessed", "check out", "tweet", "|", "see also", "e-mail", "strongbox",
+               "ad choices", "photograph", "about us", "faq", "careers", "view all", "app", "sign in", "contact us", "comment", "follow", "@", "http", "posted", "update", "staff writer", "editor"]
+    article = re.sub('[ \t\n]+' , ' ', article)
+    sentences = article.split('.')
+    new_sentences = []
+    for sen in sentences:
+        clean = True
+        for word in exclude:
+            if word in sen.lower():
+                clean = False
 
-def content_clean(article):
-    # hyphens to spaces
-    article.replace('-', ' ')
-    # strip all punctuation
-    article = article.translate(None, string.punctuation).replace('\n', ' ').replace('\t', ' ').lower()
+        if clean:
+            new_sentences.append(sen)
 
-    # dictionary words only
-    d = enchant.Dict("en_US")
+    outstr = ""
+    for sen in new_sentences:
+        if len(sen) > 5:
+            outstr += sen.strip() + '. '
+    return outstr
 
-    words = []
-    for word in filter(None, article.split(' ')):
-        if "http" in word:
-            continue
-        elif d.check(word) or word in hh_words:
-            words.append(word)
-        else:
-            rejectlist.append(word)
-    # truncate
-    return " ".join(words[:LEN_BOUND])
+rows = []
 
-for i in range(0, len(rows)):
-    rows[i] = [rows[i][0], rows[i][1], rows[i][2], " ".join(rows[i][3].split(" ")[:LEN_BOUND]), content_clean(rows[i][3])]
+for i in range(0, len(raw_rows)):
+    article_text = basic_strip(raw_rows[i][3])
+    if len(article_text) > CHAR_LEN_BOUND:
+        rows.append([raw_rows[i][0], raw_rows[i][1], raw_rows[i][2], article_text])
 
-most_common = Counter(rejectlist).most_common()
-heavy_hitters = [elt for elt in most_common if elt[1] > 4]
-
-if setpickle:
-    print "setting pickle"
-    pickle.dump([elt[0] for elt in heavy_hitters], pfd)
 
 ### CLASSIFIER CODE
 
@@ -69,42 +57,70 @@ def vecify(v):
     ts.gunning_fog(v)]
     # ts.readability_consensus(v)]
 
+
 Xtrain, Ytrain = [],[]
 Xtest, Ytest = [],[]
 
+def csr_2_list(csr):
+    return [csr[0,i] for i in range(0, csr.shape[1]) ]
+
 num_libarticles, num_consarticles = 0, 0
 
-for (_, slant, title, raw_article, _) in rows[::2]:
-    try:
-        print "ADDED TO TRAINING SET: " + title
-        Xtrain.append(vecify(raw_article))
-        # print vecify(article)
-        Ytrain.append(slant)
+tk = text.CountVectorizer()
+text_doc_matrix = tk.fit_transform([row[3] for row in rows])
+for i in range(0, text_doc_matrix.shape[0]):
 
-        if slant == 'L':
-            num_libarticles += 1
-        else:
-            num_consarticles += 1
+    if rows[i][1] == 'C':
+        num_consarticles += 1
+    else:
+        num_libarticles += 1
 
-    except:
-        print "TRAINING SET APPEND OP ERROR: " + title
+    if i % 3 == 0:
+        Xtrain.append(csr_2_list(text_doc_matrix[i]))
+        Ytrain.append(rows[i][1])
+    else:
+        Xtest.append(csr_2_list(text_doc_matrix[i]))
+        Ytest.append(rows[i][1])
+    print i
+
+
+
+
+
+
+# for (_, slant, title, raw_article) in rows[::2]:
+#     try:
+#         print "ADDED TO TRAINING SET: " + title
+#         Xtrain.append(vecify(raw_article))
+#         # print vecify(article)
+#         Ytrain.append(slant)
+
+#         if slant == 'L':
+#             num_libarticles += 1
+#         else:
+#             num_consarticles += 1
+
+#     except:
+#         print "TRAINING SET APPEND OP ERROR: " + title
 
 clf = svm.SVC()
 clf.fit(Xtrain, Ytrain)
 
-for (_, slant, title, raw_article, _) in rows[1::2]:
-    try:
-        print "ADDED TO TESTING SET: " + title
-        Xtest.append(vecify(raw_article))
-        Ytest.append(slant)
+# for (_, slant, title, raw_article) in rows[1::2]:
+#     try:
 
-        if slant == 'L':
-            num_libarticles += 1
-        else:
-            num_consarticles += 1
+#         Xtest.append(vecify(raw_article))
+#         Ytest.append(slant)
 
-    except:
-        print "TESTING SET APPEND OP ERROR: " + title
+#         if slant == 'L':
+#             num_libarticles += 1
+#         else:
+#             num_consarticles += 1
+
+#         print "ADDED TO TESTING SET: " + title
+
+#     except:
+#         print "TESTING SET APPEND OP ERROR: " + title
 
 print "The classifier was %.2f%% accurate." % (clf.score(Xtest, Ytest)*100)
 print "%d liberal articles, %d conservative articles." % (num_libarticles, num_consarticles)

080fb005db13b3c8923741f0557cf2b3a36fb4de
jombooth <josephbooth@college.harvard.edu>
2014-12-11 01:08:31 -0500

    excrement

--- web/post_series.php
+++ web/post_series.php
@@ -1,20 +1,20 @@
 <?php
 
-include_once("config.php");
-
 file_put_contents('php_log', "got here", FILE_APPEND);
 
-if (isset($_POST['user_agent']) && isset($_POST['time_series'])) {
-    $user_agent = mysql_real_escape_string($_POST['user_agent']);
-    $time_series = mysql_real_escape_string($_POST['time_series']);
-    file_put_contents('php_log_query', $user_agent);
-    //file_put_contents('php_log_query', $time_series, FILE_APPEND);
-    $qs = "INSERT INTO ts_table (UserAgent, TimeSeries) VALUES (\"" + $user_agent + "\", \"" + $time_series + "\")";
+if (isset($_POST['data'])) {
 
-    //file_put_contents('php_log_query', $qs, FILE_APPEND);
 
-    $mysqli->query($qs);
-    $mysqli->close();
+
+    $ipt = str_replace($_POST['data'] , "'");
+
+    file_put_contents('escaped_string', $ipt);
+
+    $result = exec('echo "' . $_POST['data'] . '" | python quickClassify.py')
+
+    file_put_contents('php_result', $result, FILE_APPEND);
+
+    print $result;
 }
 
 ?>

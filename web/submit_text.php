8acc37b3381b446a193a0e4a762398a2079d3e73
josboo6 <jombooth@gmail.com>
2014-12-11 19:08:13 -0800

    removing useless files

--- web/submit_text.php
+++ web/submit_text.php
@@ -2,10 +2,17 @@
 
 file_put_contents('php_log', "got here", FILE_APPEND);
 
-if (!isset($_POST['data'])) {
+if (isset($_POST['data'])) {
 
-
-    $_POST['data'] = "George dubya \" sure bunged up this one";
+    if (strlen($_POST['data']) < 100) {
+        print "Input string was below the 100 character minimum!";
+        die();
+    }
+    else if (strlen($_POST['data']) > 10000000) {
+        print "Query larger than 1MB. Dying.";
+        die();
+    }
+    // $_POST['data'] = "George dubya \" sure bunged up this one";
     $ipt = str_replace('"', "'", $_POST['data']);
 
     file_put_contents('escaped_string', $ipt);
